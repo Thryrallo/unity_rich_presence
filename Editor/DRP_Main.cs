f1cfdad1638078821060c712aebf6ee7fa48a114
Thryrallo <40315315+Thryrallo@users.noreply.github.com>
2019-09-08 18:44:34 +0200

    Update DRP_Main.cs

--- Editor/DRP_Main.cs
+++ Editor/DRP_Main.cs
@@ -84,6 +84,7 @@ namespace Thry
             ModuleHandler.RegisterPreModuleRemoveFunction(delegate ()
             {
                 EditorApplication.update -= Update;
+                discord.Dispose();
             });
         }
 
